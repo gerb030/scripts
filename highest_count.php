496c19e68cec63f51998ae2f1f48ae582d6e61d7
Gerb <gerben.degraaf@spilgames.com>
2013-01-04 09:22:20 +0100

    Fixed the measuring date

--- highest_count.php
+++ highest_count.php
@@ -1,4 +1,4 @@
-#!/opt/local/bin/php
+#!/usr/bin/php
 <?php
 // put in your favourite json data file and get the peak concurrent players daily
 date_default_timezone_set("Europe/Amsterdam");
@@ -9,7 +9,7 @@ foreach($json[0]['datapoints'] as $range) {
     $date = date('j F Y', $range[1]);
     if (!isset($allData[$date]))
         $allData[$date] = array('users' => round($range[0]), 'date' => date('H:i \o\n j F Y', $range[1]));
-    else if ($numUsers > $allData[$date]['users']) $allData[$date]['users'] = round($numUsers);
+    else if ($numUsers > $allData[$date]['users']) $allData[$date] = array('users' => round($range[0]), 'date' => date('H:i \o\n j F Y', $range[1]));
 }
 foreach($allData as $block) echo 'At '.$block['date'].', concurrent users peaked at '.$block['users'].".\n";
 exit(0);
